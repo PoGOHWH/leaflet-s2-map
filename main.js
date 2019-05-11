c84193f3b3cbc605576cdbdbdf233187a98de5d2
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2019-05-11 14:21:59 +0530

    Attribution added

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ L.control.locate({
 L.tileLayer(
   'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     accessToken,
-    attribution: '',
+    attribution: 'map tiles &copy; <a href="https://www.openstreetmap.org/">Mapbox</a>, <a href="https://creativecommons.org/licenses/by/3.0/us/">CC-BY</a> | map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> | dataset from <a href="https://github.com/PoGOHWH/iitc-pogo-json">pogohwh/iitc-pogo-json</a>',
     maxZoom: 18,
   }).addTo(map)
 
